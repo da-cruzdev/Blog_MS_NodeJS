2d5d9d518122882dbe598f78a8a148aeae2b39b4
Da-cruz <wilfrieddacruz@arolitec.com>
2023-03-24 12:03:34 +0000

    create Admin when blog is setup

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,13 +1,69 @@
-const blogOne = async (req, res) => {
-  res.send("Blog 1");
+const BlogSeting = require("../models/blogSettingModel");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+
+const securePassword = async (password) => {
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
+    return passwordHash;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
-const blogTwo = async (req, res) => {
-  res.send("Blog 2");
+const login = async (req, res) => {
+  res.send("Login here");
 };
 
 const blogSetup = async (req, res) => {
-  res.send("Blog Setup here");
+  try {
+    const blogSetting = await BlogSeting.find({});
+    if (blogSetting > 0) {
+      res.redirect("/login");
+    } else {
+      res.render("blogSetup");
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-module.exports = { blogOne, blogTwo, blogSetup };
+const CreateblogSetup = async (req, res) => {
+  try {
+    const blog_title = req.body.blog_title;
+    const description = req.body.description;
+    const name = req.body.name;
+    const email = req.body.email;
+    const blog_image = req.file.filename;
+    const password = await securePassword(req.body.password);
+    console.log(req.body.password);
+    console.log(email);
+
+    const blogSetting = new BlogSeting({
+      blog_title: blog_title,
+      blog_logo: blog_image,
+      description: description,
+    });
+
+    await blogSetting.save();
+
+    const user = new User({
+      name: name,
+      email: email,
+      password: password,
+      isAdmin: 1,
+    });
+    console.log(user);
+
+    const userAdmin = await user.save();
+    if (userAdmin) {
+      res.redirect("/login");
+    } else {
+      res.render("/blogSetup", { message: "BLog not setup properly" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { login, blogSetup, CreateblogSetup };
--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,9 +1,49 @@
 const express = require("express");
 const adminController = require("../controllers/adminController");
 const adminRoute = express();
+const bodyParser = require("body-parser");
 
-adminRoute.get("/", adminController.blogOne);
-adminRoute.get("/blog", adminController.blogTwo);
+adminRoute.use(bodyParser.json());
+adminRoute.use(bodyParser.urlencoded({ extended: true }));
+
+adminRoute.set("view engine", "ejs");
+adminRoute.set("views", "./views");
+
+const multer = require("multer");
+const path = require("path");
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, path.join(__dirname, "../public/images/"));
+//   },
+//   filename: function (req, file, cb) {
+//     const name = Date.now() + "-" + file.originalname;
+//     cb(null, name);
+//   },
+// });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/images/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+adminRoute.use(express.static("public"));
+
+adminRoute.get("/login", adminController.login);
 adminRoute.get("/blog-setup", adminController.blogSetup);
+adminRoute.post(
+  "/blog-setup",
+  upload.single("blog_image"),
+  adminController.CreateblogSetup
+);
 
 module.exports = adminRoute;
